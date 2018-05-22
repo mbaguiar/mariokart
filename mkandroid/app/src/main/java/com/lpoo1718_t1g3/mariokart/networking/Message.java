c870493baac5aa32506da3b73027337cd5f84269
Tiago Fragoso <tiagofragoso@outlook.com>
2018-05-22 12:40:21 +0100

    Updated Message class to match desktop one

--- mkandroid/app/src/main/java/com/lpoo1718_t1g3/mariokart/networking/Message.java
+++ mkandroid/app/src/main/java/com/lpoo1718_t1g3/mariokart/networking/Message.java
@@ -9,16 +9,18 @@ public class Message implements Serializable {
 
     private SENDER sender;
 
+    private int senderId;
+
     private Hashtable<String, Object> options;
 
-    public enum MESSAGE_TYPE {CONNECTION, CONTROLLER_ACTIVITY, CHAR_PICK, TRACK_VOTE}
+    public enum MESSAGE_TYPE {CONNECTION, PLAYER_REGISTRY, DISCONNECTION, CONTROLLER_ACTIVITY, CHAR_PICK, TRACK_VOTE, STATE_UPDATE}
 
     public enum SENDER {SERVER, CLIENT}
 
     public Message(MESSAGE_TYPE t, SENDER s){
         type = t;
         sender = s;
-        options = new Hashtable<>();
+        options = new Hashtable<String, Object>();
     }
 
     public MESSAGE_TYPE getType(){
@@ -33,10 +35,18 @@ public class Message implements Serializable {
         return options;
     }
 
+    public int getSenderId() { return this.senderId; }
+
+    public void setSenderId(int id){
+        this.senderId = id;
+    }
+
     @Override
     public String toString() {
         return "Message{" +
                 "type=" + type +
+                ", sender=" + sender +
+                ", senderId=" + senderId +
                 ", options=" + options +
                 '}';
     }
