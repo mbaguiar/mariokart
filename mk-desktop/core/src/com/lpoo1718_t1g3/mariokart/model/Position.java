a1a5f64da7839e50e0ea563d15c19a074c8553b5
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2018-05-31 23:50:45 +0100

    Added table with players info

--- mk-desktop/core/src/com/lpoo1718_t1g3/mariokart/model/Position.java
+++ mk-desktop/core/src/com/lpoo1718_t1g3/mariokart/model/Position.java
@@ -7,17 +7,23 @@ public class Position {
     public int laps;
     public long time;
     public int playerId;
+    public String description;
 
     Position(int playerId) {
         this.laps = -1;
         this.time = 0;
         this.playerId = playerId;
+        this.description = "0/" + TrackModel.LAPS + " laps";
     }
 
 
     public void incLaps() {
         this.laps++;
         this.time = System.currentTimeMillis();
+        description = laps+1 + "/" + TrackModel.LAPS + " laps";
+        if (isFinished()) {
+            description = "Finished!";
+        }
     }
 
     public int getPlayerId() {
--- mk-desktop/core/src/com/lpoo1718_t1g3/mariokart/view/RaceView.java
+++ mk-desktop/core/src/com/lpoo1718_t1g3/mariokart/view/RaceView.java
@@ -3,16 +3,23 @@ package com.lpoo1718_t1g3.mariokart.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
 import com.lpoo1718_t1g3.mariokart.MarioKart;
 import com.lpoo1718_t1g3.mariokart.controller.GameController;
 import com.lpoo1718_t1g3.mariokart.controller.entities.KartBody;
 import com.lpoo1718_t1g3.mariokart.model.GameModel;
 import com.lpoo1718_t1g3.mariokart.model.Player;
+import com.lpoo1718_t1g3.mariokart.model.Position;
 import com.lpoo1718_t1g3.mariokart.model.entities.BananaModel;
 import com.lpoo1718_t1g3.mariokart.model.entities.EntityModel;
 import com.lpoo1718_t1g3.mariokart.model.entities.MysteryBoxModel;
@@ -31,10 +38,17 @@ public class RaceView extends ScreenAdapter {
     private MysteryBoxView mysteryBoxView;
     private TrackView trackView;
     private OrthographicCamera camera;
+    private Label.LabelStyle labelStyle;
+    private Label.LabelStyle labelStyleSmall;
+    private Table connectedPlayers;
+    private HashMap<Integer, Image> place_numbers = new HashMap<Integer, Image>();
+    private HashMap<String, Image> characters_symbols = new HashMap<String, Image>();
+
     public static final float PIXEL_TO_METER = 0.04f;
     public static final float VIEWPORT_WIDTH = 76.8f;
     public static final float VIEWPORT_HEIGHT = 43.2f;
-    boolean mario = false, luigi = false;
+
+    private Stage stage;
 
     Box2DDebugRenderer debugRenderer;
 
@@ -45,7 +59,89 @@ public class RaceView extends ScreenAdapter {
         camera = createCamera();
         initKartViews();
         initObjectViews();
+
+        this.stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        stage.setDebugAll(false);
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("SuperMario256.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.borderColor = Color.BLACK;
+        parameter.borderWidth = 5;
+        parameter.size = 100;
+
+        labelStyle = new Label.LabelStyle();
+        labelStyle.font = generator.generateFont(parameter);
+        Label lobbyLabel = new Label("Players", labelStyle);
+
+        lobbyLabel.setPosition(1080 + 840/2, stage.getHeight() * 0.9f, Align.center);
+        setUpPlaceNumbers();
+        setUpCharacterSymbols();
+
+        connectedPlayers = new Table();
+        connectedPlayers.setDebug(false);
+
+        connectedPlayers.setPosition(1080 + 840/2, stage.getHeight() * 0.45f, Align.top);
+
+        stage.addActor(connectedPlayers);
+        stage.addActor(lobbyLabel);
+
+        parameter.size = 40;
+        labelStyle.font = generator.generateFont(parameter);
+
+        parameter.size = 30;
+        parameter.color = Color.GOLD;
+        parameter.borderColor = Color.BROWN;
+        parameter.borderWidth = 4;
+
+        labelStyleSmall = new Label.LabelStyle();
+        labelStyleSmall.font = generator.generateFont(parameter);
+
         debugRenderer = new Box2DDebugRenderer(true, true, false, true, true, true);
+
+    }
+
+    private void setUpCharacterSymbols() {
+        Texture texture = new Texture(Gdx.files.internal("mario_symbol.png"));
+        Image image = new Image(texture);
+        characters_symbols.put("Mario", image);
+        texture = new Texture(Gdx.files.internal("luigi_symbol.png"));
+        image = new Image(texture);
+        characters_symbols.put("Luigi", image);
+        texture = new Texture(Gdx.files.internal("peach_symbol.png"));
+        image = new Image(texture);
+        characters_symbols.put("Peach", image);
+        texture = new Texture(Gdx.files.internal("toad_symbol.png"));
+        image = new Image(texture);
+        characters_symbols.put("Toad", image);
+        texture = new Texture(Gdx.files.internal("yoshi_symbol.png"));
+        image = new Image(texture);
+        characters_symbols.put("Yoshi", image);
+        texture = new Texture(Gdx.files.internal("bowser_symbol.png"));
+        image = new Image(texture);
+        characters_symbols.put("Bowser", image);
+    }
+
+    private void setUpPlaceNumbers() {
+        Texture texture = new Texture(Gdx.files.internal("1_place.png"));
+        Image image = new Image(texture);
+        place_numbers.put(1, image);
+        texture = new Texture(Gdx.files.internal("2_place.png"));
+        image = new Image(texture);
+        place_numbers.put(2, image);
+        texture = new Texture(Gdx.files.internal("3_place.png"));
+        image = new Image(texture);
+        place_numbers.put(3, image);
+        texture = new Texture(Gdx.files.internal("4_place.png"));
+        image = new Image(texture);
+        place_numbers.put(4, image);
+        texture = new Texture(Gdx.files.internal("5_place.png"));
+        image = new Image(texture);
+        place_numbers.put(5, image);
+        texture = new Texture(Gdx.files.internal("6_place.png"));
+        image = new Image(texture);
+        place_numbers.put(6, image);
 
     }
 
@@ -71,20 +167,48 @@ public class RaceView extends ScreenAdapter {
         MarioKart.getInstance().getAssetManager().finishLoading();
     }
 
+    private void reloadTable(Label.LabelStyle labelStyle, Label.LabelStyle labelStyleSmall) {
+
+
+
+        connectedPlayers.clearChildren();
+        int t = 1;
+        for (Position p : GameModel.getInstance().getCurrentRace().getPlayerPositions()) {
+            Player player = GameModel.getInstance().getPlayer(p.playerId);
+            Label playerLabel = new Label(player.getPlayerHandle(), labelStyle);
+            connectedPlayers.add(place_numbers.get(t)).width(70).height(70).pad(20).padBottom(0);
+            connectedPlayers.add(playerLabel).pad(20).padBottom(0);
+            connectedPlayers.add(characters_symbols.get(player.getSelectedCharacter().getName())).width(70).height(70).padBottom(0);
+            t++;
+            connectedPlayers.row();
+            playerLabel = new Label("", labelStyle);
+            connectedPlayers.add(playerLabel).padBottom(0);
+            playerLabel = new Label(p.description, labelStyleSmall);
+            connectedPlayers.add(playerLabel).padBottom(0);
+            connectedPlayers.row();
+        }
+    }
+
     @Override
     public void render(float delta) {
         handleInputs(delta);
+
+        reloadTable(this.labelStyle, this.labelStyleSmall);
 
         GameController.getInstance().update(delta);
         camera.update();
         MarioKart.getInstance().getBatch().setProjectionMatrix(camera.combined);
 
-        Gdx.gl.glClearColor(1, 1, 1f, 1);
+        Gdx.gl.glClearColor(96 / 255f,96 / 255f, 96 / 255f, 0.5f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         MarioKart.getInstance().getBatch().begin();
+
         drawEntities();
         MarioKart.getInstance().getBatch().end();
+
+        stage.act();
+        stage.draw();
 
 
         //debugRenderer.render(GameController.getInstance().getRaceController().getWorld(), camera.combined);
